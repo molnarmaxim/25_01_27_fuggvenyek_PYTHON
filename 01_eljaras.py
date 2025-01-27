b7de9f19233916e45af6486d26615761a006f1c9
Molnár "Scopsy" Maxim <molnar.maxim@diak.szbi-pg.hu>
2025-01-27 08:46:40 +0100

    update

--- 01_eljaras.py
+++ 01_eljaras.py
@@ -1,4 +1,4 @@
 def koszont_ket_nevvel(nev1, nev2):
-    print("Szia" + nev1 + ", " + nev2 + "\!nÜdv a fedélzeten!")
+    print("Szia " + nev1 + ", " + nev2 + "!\nÜdv a fedélzeten!")
 
-koszont_ket_nevvel(nev1, nev2)
+koszont_ket_nevvel("Bulcsu", "Geci")
---
+++ 02_eljaras.py
@@ -0,0 +1,7 @@
+def osszead(x, y):
+    eredmeny = x + y
+    print("A két szám összege: ", eredmeny)
+
+osszead(10, 9)
+osszead(5+5, 5+4)
+
